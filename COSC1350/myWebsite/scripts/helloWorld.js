d5803ba23cd19aa124c0c592c5709181e91d7570
Lindsey Michelle Yaezenko <lindseyburchett@westernwyoming.edu>
2024-09-07 14:55:00 -0600

    Add files via upload

---
+++ COSC1350/myWebsite/scripts/helloWorld.js
@@ -0,0 +1,8 @@
+/*
+    Student Name: Lindsey Yaezenko
+    File Name: helloWorld.js
+    Date: 09.02.2024
+*/
+
+let hello ="Hello World";
+console.log(hello);
---
+++ COSC1350/myWebsite/scripts/practiceVariables.js
@@ -0,0 +1,24 @@
+/*
+    Student Name: Lindsey Yaezenko
+    File Name: practiceVariables.js
+    Date: 09/03/2024
+    */
+
+ let hiLindsey = "Hi Lindsey!";
+ console.log(typeof hiLindsey);
+ let fourPointFive = "4.5";
+ console.log(typeof fourPointFive);
+ let zero= "0";
+ console.log(typeof zero);
+ let negativeTen = "-10";
+ console.log(typeof negativeTen);
+ let ifTrue = "true";
+ console.log(typeof ifTrue);
+ console.log("Hi Lindsey!\nLet's get started!");
+ console.log(`half of 100 is ${100/2}`);
+ console.log("con"+"cat"+"e"+"nate");
+ console.log(8*null);
+ console.log("5"-1);
+ console.log("5"+1);
+ console.log("five"*2);
+ console.log(false==true)
