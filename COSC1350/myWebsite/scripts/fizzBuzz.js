d2a9c1e1caa0b84eb846160d4dc051e5e30914ef
Lindsey Michelle Yaezenko <lindseyburchett@westernwyoming.edu>
2024-09-14 18:52:23 -0600

    Add files via upload

---
+++ COSC1350/myWebsite/scripts/fizzBuzz.js
@@ -0,0 +1,18 @@
+/*
+    Student Name: Lindsey Yaezenko
+    File Name: fizzBuzz.js
+    Date: 09/14/2024
+    */
+
+    for ( let num = 1; num <= 100; num+=1 ) {
+         if ( num % 3 == 0 ) {
+          console.log( "Fizz" );
+        } 
+         else if ( num % 5 == 0 ) {
+          console.log( "Buzz" );
+        } 
+          else {
+          console.log( num );
+        }
+      }
+      
